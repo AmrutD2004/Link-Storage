13e3d7fdd3914d30003c15ce61f27ee409633646
Amrut Deshpande <amrutdeshpande2004@gmail.com>
2026-03-11 20:40:31 +0530

    added the new backend Link

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,6 +49,7 @@ AUTH_USER_MODEL = 'links.Users'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
